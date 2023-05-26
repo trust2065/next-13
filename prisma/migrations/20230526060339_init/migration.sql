ddff32e0476f909cde42fd3489e66250c4846898
Choco <trust2065@gmail.com>
2023-05-26 16:08:21 +1000

    Simple todo

--- prisma/migrations/20230526060339_init/migration.sql
+++ prisma/migrations/20230526060339_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "Todo" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
-    "complte" BOOLEAN NOT NULL,
+    "complete" BOOLEAN NOT NULL,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
